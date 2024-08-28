81da26fd0dc483846a7d85bf7ce7535d92e86183
mvishiu11 <jakubmuszynski2103@gmail.com>
2024-08-28 22:16:00 +0800

    feat: expand the interpreter to cover other literals

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -3,46 +3,78 @@ use crate::{error::EvalError, expr::{Expr, LiteralExpr}, token::TokenType};
 
 impl Error for EvalError {}
 
-// Define the `interpret` function
-pub fn interpret(expr: &Expr) -> Result<f64, EvalError> {
-    match expr {
-        Expr::Literal(literal) => match literal {
-            LiteralExpr::Number(n) => Ok(*n),
-            LiteralExpr::String(_) => Err(EvalError::TypeError("Cannot interpret string as number".to_string())),
-            LiteralExpr::Boolean(_) => Err(EvalError::TypeError("Cannot interpret boolean as number".to_string())),
-            LiteralExpr::Nil => Err(EvalError::TypeError("Cannot interpret nil as number".to_string())),
+// Function to print or handle the result of an expression
+pub fn interpret(expr: &Expr) -> Result<(), EvalError> {
+    match evaluate(expr)? {
+        Expr::Literal(literal) => {
+            match literal {
+                LiteralExpr::Number(n) => println!("{}", n),
+                LiteralExpr::String(s) => println!("{}", s),
+                LiteralExpr::Boolean(b) => println!("{}", b),
+                LiteralExpr::Nil => println!("Nil"),
+            }
+            Ok(())
         },
+        // Handle other types of expressions if needed
+        _ => Err(EvalError::TypeError("Unhandled expression type".to_string())),
+    }
+}
+
+// Define the `evaluate` function
+pub fn evaluate(expr: &Expr) -> Result<Expr, EvalError> {
+    match expr {
+        Expr::Literal(literal) => Ok(Expr::Literal(literal.clone())), // Return the literal as-is
         Expr::Unary(unary) => {
-            let right = interpret(&unary.right)?;
-            match unary.operator.token_type {
-                TokenType::Minus => Ok(-right),
-                TokenType::Bang => Ok(if right == 0.0 { 1.0 } else { 0.0 }), // Logical NOT
-                _ => Err(EvalError::SyntaxError("Unknown unary operator".to_string())),
+            let right = evaluate(&unary.right)?;
+            match right {
+                Expr::Literal(LiteralExpr::Number(n)) => match unary.operator.token_type {
+                    TokenType::Minus => Ok(Expr::Literal(LiteralExpr::Number(-n))),
+                    TokenType::Bang => Ok(Expr::Literal(LiteralExpr::Boolean(n == 0.0))),
+                    _ => Err(EvalError::SyntaxError("Unknown unary operator".to_string())),
+                },
+                Expr::Literal(LiteralExpr::Boolean(b)) => match unary.operator.token_type {
+                    TokenType::Bang => Ok(Expr::Literal(LiteralExpr::Boolean(!b))),
+                    _ => Err(EvalError::SyntaxError("Unknown unary operator".to_string())),
+                },
+                _ => Err(EvalError::TypeError("Cannot apply unary operator to non-numeric or non-boolean type".to_string())),
             }
         },
         Expr::Binary(binary) => {
-            let left = interpret(&binary.left)?;
-            let right = interpret(&binary.right)?;
-            match binary.operator.token_type {
-                TokenType::Plus => Ok(left + right),
-                TokenType::Minus => Ok(left - right),
-                TokenType::Star => Ok(left * right),
-                TokenType::Slash => {
-                    if right == 0.0 {
+            let left = evaluate(&binary.left)?;
+            let right = evaluate(&binary.right)?;
+            match (left, right) {
+                (Expr::Literal(LiteralExpr::Number(l)), Expr::Literal(LiteralExpr::Number(r))) => match binary.operator.token_type {
+                    TokenType::Plus => Ok(Expr::Literal(LiteralExpr::Number(l + r))),
+                    TokenType::Minus => Ok(Expr::Literal(LiteralExpr::Number(l - r))),
+                    TokenType::Star => Ok(Expr::Literal(LiteralExpr::Number(l * r))),
+                    TokenType::Slash => if r == 0.0 {
                         Err(EvalError::DivisionByZero)
                     } else {
-                        Ok(left / right)
-                    }
+                        Ok(Expr::Literal(LiteralExpr::Number(l / r)))
+                    },
+                    TokenType::EqualEqual => Ok(Expr::Literal(LiteralExpr::Boolean(l == r))),
+                    TokenType::BangEqual => Ok(Expr::Literal(LiteralExpr::Boolean(l != r))),
+                    TokenType::Greater => Ok(Expr::Literal(LiteralExpr::Boolean(l > r))),
+                    TokenType::GreaterEqual => Ok(Expr::Literal(LiteralExpr::Boolean(l >= r))),
+                    TokenType::Less => Ok(Expr::Literal(LiteralExpr::Boolean(l < r))),
+                    TokenType::LessEqual => Ok(Expr::Literal(LiteralExpr::Boolean(l <= r))),
+                    _ => Err(EvalError::SyntaxError("Unknown binary operator".to_string())),
                 },
-                TokenType::EqualEqual => Ok(if left == right { 1.0 } else { 0.0 }),
-                TokenType::BangEqual => Ok(if left != right { 1.0 } else { 0.0 }),
-                TokenType::Greater => Ok(if left > right { 1.0 } else { 0.0 }),
-                TokenType::GreaterEqual => Ok(if left >= right { 1.0 } else { 0.0 }),
-                TokenType::Less => Ok(if left < right { 1.0 } else { 0.0 }),
-                TokenType::LessEqual => Ok(if left <= right { 1.0 } else { 0.0 }),
-                _ => Err(EvalError::SyntaxError("Unknown binary operator".to_string())),
+                (Expr::Literal(LiteralExpr::String(l)), Expr::Literal(LiteralExpr::String(r))) => match binary.operator.token_type {
+                    TokenType::Plus => Ok(Expr::Literal(LiteralExpr::String(l + &r))),
+                    _ => Err(EvalError::TypeError("Unsupported operation for strings".to_string())),
+                },
+                (Expr::Literal(LiteralExpr::Number(l)), Expr::Literal(LiteralExpr::String(r))) => match binary.operator.token_type {
+                    TokenType::Plus => Ok(Expr::Literal(LiteralExpr::String(format!("{}{}", l, r)))),
+                    _ => Err(EvalError::TypeError("Unsupported operation for mixed types".to_string())),
+                },
+                (Expr::Literal(LiteralExpr::String(l)), Expr::Literal(LiteralExpr::Number(r))) => match binary.operator.token_type {
+                    TokenType::Plus => Ok(Expr::Literal(LiteralExpr::String(format!("{}{}", l, r)))),
+                    _ => Err(EvalError::TypeError("Unsupported operation for mixed types".to_string())),
+                },
+                _ => Err(EvalError::TypeError("Operands must be compatible for the operation".to_string())),
             }
         },
-        Expr::Grouping(grouping) => interpret(&**grouping),
+        Expr::Grouping(grouping) => evaluate(&**grouping),
     }
 }
