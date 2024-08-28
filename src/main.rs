8a93ad9c1cbca3a303a0aaabf3b6c7e36ec568d0
mvishiu11 <jakubmuszynski2103@gmail.com>
2024-08-28 21:36:03 +0800

    feat: wire-up the interpreter, add cli option

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,37 @@ mod token;
 mod expr;
 mod parser;
 mod error;
+mod interpreter;
 
 use lexer::Lexer;
 use std::env;
 use std::fs;
 use std::io::{self, Write};
 
+const TOKENIZE: &str = "tokenize";
+const PARSE: &str = "parse";
+const INTERPRET: &str = "interpret";
+const CLI: &str = "cli";
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
+    if args.len() < 2 {
+        writeln!(io::stderr(), "Usage: {} <command> <filename>", args[0]).unwrap();
+        writeln!(io::stderr(), "Commands: {TOKENIZE} {PARSE} {INTERPRET} {CLI}").unwrap();
         return;
     }
 
     let command = &args[1];
-    let filename = &args[2];
+    let mut filename = "";
+    if command == CLI {
+        println!("🚀 Welcome to the Lox programming language REPL!");
+    }
+    else {
+        filename = &args[2];
+    }
 
     match command.as_str() {
-        "tokenize" => {
+        TOKENIZE => {
             writeln!(io::stdout(), "✨ Program logs will be displayed here. Stay tuned!").unwrap();
 
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
@@ -38,7 +51,7 @@ fn main() {
                 println!("EOF  null");
             }
         }
-        "parse" => {
+        PARSE => {
             writeln!(io::stdout(), "✨ Program logs will be displayed here. Stay tuned!").unwrap();
 
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
@@ -58,8 +71,58 @@ fn main() {
                 println!("EOF  null");
             }
         }
+        INTERPRET => {
+            writeln!(io::stdout(), "✨ Program logs will be displayed here. Stay tuned!").unwrap();
+
+            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
+                writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
+                String::new()
+            });
+
+            if !file_contents.is_empty() {
+                let mut lexer = Lexer::new(file_contents);
+                let tokens = lexer.tokenize();
+                let mut parser = parser::Parser::new(tokens.to_vec());
+                match parser.parse() {
+                    Ok(expr) => match interpreter::interpret(&expr) {
+                        Ok(result) => println!("{}", result),
+                        Err(e) => writeln!(io::stderr(), "{}", e).unwrap(),
+                    },
+                    Err(e) => writeln!(io::stderr(), "{}", e).unwrap(),
+                }
+            } else {
+                println!("EOF  null");
+            }
+        }
+        CLI => {
+            writeln!(io::stdout(), "✨ Program logs will be displayed here. Stay tuned!").unwrap();
+
+            let mut input = String::new();
+            loop {
+                print!("> ");
+                io::stdout().flush().unwrap();
+                io::stdin().read_line(&mut input).unwrap();
+                if input.trim() == "exit" {
+                    break;
+                }
+
+                let mut lexer = Lexer::new(input.clone());
+                let tokens = lexer.tokenize();
+                let mut parser = parser::Parser::new(tokens.to_vec());
+                match parser.parse() {
+                    Ok(expr) => match interpreter::interpret(&expr) {
+                        Ok(result) => println!("{}", result),
+                        Err(e) => writeln!(io::stderr(), "{}", e).unwrap(),
+                    },
+                    Err(e) => writeln!(io::stderr(), "{}", e).unwrap(),
+                }
+
+                input.clear();
+            }
+        }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
+            writeln!(io::stderr(), "Commands: {TOKENIZE} {PARSE} {INTERPRET} {CLI}").unwrap();
             return;
         }
     }
