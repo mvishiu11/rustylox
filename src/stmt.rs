ed1e2d86563fa113c7ac9200c06f9fb2db6d0c12
mvishiu11 <jakubmuszynski2103@gmail.com>
2024-09-03 15:32:50 +0800

    feat: add statement structure

---
+++ src/stmt.rs
@@ -0,0 +1,12 @@
+use crate::expr::Expr;
+
+#[derive(Debug, Clone)]
+#[allow(dead_code)]
+pub enum Stmt {
+    Expression(Expr),
+    Print(Expr),
+    Var(String, Option<Expr>),
+    Block(Vec<Stmt>),
+    If(Expr, Box<Stmt>, Option<Box<Stmt>>),
+    While(Expr, Box<Stmt>),
+}
