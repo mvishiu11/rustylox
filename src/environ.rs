37b6661eb7a5cea5e44d7a1aed76640054aab3a5
mvishiu11 <jakubmuszynski2103@gmail.com>
2024-09-04 14:24:33 +0800

    feat: modify environ to use shared refs

--- src/environ.rs
+++ src/environ.rs
@@ -1,15 +1,16 @@
+use std::cell::RefCell;
 use std::collections::HashMap;
+use std::rc::Rc;
 use crate::expr::LiteralExpr;
 use crate::error::EvalError;
 use crate::token::Token;
 
 #[derive(Debug, Clone)]
 pub struct Environment {
-    enclosing: Option<Box<Environment>>,
+    pub enclosing: Option<Rc<RefCell<Environment>>>,
     values: HashMap<String, LiteralExpr>,
 }
 
-#[allow(dead_code)]
 impl Environment {
     pub fn new() -> Self {
         Environment {
@@ -18,9 +19,9 @@ impl Environment {
         }
     }
 
-    pub fn new_enclosed(enclosing: Environment) -> Self {
+    pub fn new_enclosed(enclosing: Rc<RefCell<Environment>>) -> Self {
         Environment {
-            enclosing: Some(Box::new(enclosing)),
+            enclosing: Some(enclosing),
             values: HashMap::new(),
         }
     }
@@ -34,21 +35,20 @@ impl Environment {
             self.values.insert(name.lexeme.clone(), value);
             Ok(())
         } else {
-            if let Some(enclosing) = &mut self.enclosing {
-                enclosing.assign(name, value)
+            if let Some(enclosing) = &self.enclosing {
+                enclosing.borrow_mut().assign(name, value)
             } else {
                 Err(EvalError::UndefinedVariable(name.lexeme.clone()))
             }
         }
     }
 
-    // Get the value of a variable or throw Runtime Error if not exists
     pub fn get(&self, name: &Token) -> Result<LiteralExpr, EvalError> {
         if let Some(value) = self.values.get(&name.lexeme) {
             Ok(value.clone())
         } else {
             if let Some(enclosing) = &self.enclosing {
-                enclosing.get(name)
+                enclosing.borrow().get(name)
             } else {
                 Err(EvalError::UndefinedVariable(name.lexeme.clone()))
             }
